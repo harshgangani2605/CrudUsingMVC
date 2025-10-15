d31d4c0dbc1742533b0e566637e6b88a9ac9da04
Harsh Gangnai <harshgangani2004@gmail.com>
2025-10-15 06:04:34 -0700

    Add swagger clients api

---
+++ Controllers/ClientsApiController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp1.Data;
+using MyApp1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyApp1.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientsApiController : ControllerBase
+    {
+        private readonly MyAppContext _context;
+
+        public ClientsApiController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        // GET: api/clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+                return NotFound();
+
+            return client;
+        }
+
+        // POST: api/clients
+        [HttpPost]
+        public async Task<ActionResult<Client>> CreateClient([FromBody] Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetClient), new { id = client.Id }, client);
+        }
+
+        // PUT: api/clients/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateClient(int id, [FromBody] Client client)
+        {
+            if (id != client.Id)
+                return BadRequest();
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Clients.Any(e => e.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/clients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+                return NotFound();
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
